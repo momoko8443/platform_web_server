1784a86530f05fc0114f60bfb560300b58483080
Jake.Zheng <Huibin.zheng@hpe.com>
2019-05-29 22:54:28 +0800

    up

--- startup/stop.sh
+++ startup/stop.sh
@@ -4,7 +4,8 @@ PIDFILE="./startup/process.pid"  # PID文件路径
 echo "stopping platform_web_server" 
 if [ -f $PIDFILE ]; then
     echo "pid file exists...." 
-    PID=$(cat $PIDFILE)           # 将PID从文件中读取，并作为一个变量
-    sudo kill -QUIT $PID
+    PID=$(cat $PIDFILE) 
+    echo $PID          # 将PID从文件中读取，并作为一个变量
+    sudo kill -9 $PID
     echo "done!"
 fi
