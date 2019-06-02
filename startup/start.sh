1cf8d0ae5ae6cf04f5c77bf0f24454c98257ea44
Jake.Zheng <Huibin.zheng@hpe.com>
2019-06-02 23:11:57 +0800

    up

--- startup/start.sh
+++ startup/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "starting platform_web_server"
-nohup DOMAIN=47.111.18.121 npm run start  & echo $! > ./startup/process.pid
+nohup npm run start & echo $! > ./startup/process.pid
 echo "done!"
