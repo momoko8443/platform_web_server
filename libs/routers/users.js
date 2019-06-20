d70e5abbe5bfc873d419f25a26c605e7367bd998
Jake.Zheng <Huibin.zheng@hpe.com>
2019-06-20 17:16:01 +0800

    update

--- libs/routers/users.js
+++ libs/routers/users.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
 const rp = require('request-promise');
 const usersApi = new Router();
+let idm_domain = process.env.IDM? process.env.IDM : "47.111.18.39:5200";
 function autoParse(body, response, resolveWithFullResponse) {
     if (response.headers['content-type'] && response.headers['content-type'].search('application/json') > -1) {
         return JSON.parse(body);
@@ -10,7 +11,25 @@ function autoParse(body, response, resolveWithFullResponse) {
 }
 let request = rp.defaults({transform:autoParse});
 
-const url = `http://47.111.18.121:8011/api-upms/saasuser/memberlist`;
+const url = `http://${idm_domain}/api-user/saasuser/memberlist`;
+/**
+ * @api {get} /benyun/api/users
+ * @apiDescription 根据用户名搜索平台下的用户列表
+ * @apiName searchUsers
+ * @apiGroup Users
+ * @apiParam (queryParams) {String} username 用户名
+ * @apiParam (queryParams) {Number} pageSize 页面大小
+ * @apiParam (queryParams) {Number} currentPage 当前页码
+ * @apiSuccess {json} result
+ * @apiSuccessExample {json} Success-Response:
+ *  {
+ *      "success" : "true",
+ *      "result" : {
+ *          "name" : "loginName",
+ *          "password" : "loginPass"
+ *      }
+ *  }
+ */
 usersApi.get('/', async (ctx)=>{
     let query = ctx.query;
     let body  = await request.post(url,{
